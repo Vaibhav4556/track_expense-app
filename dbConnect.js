142fef9582d944cd202c536afb62dcd5d740f63b
Vaibhav4556 <burdevaibhav786@gmail.com>
2023-01-04 13:11:33 +0530

    init1

---
+++ dbConnect.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.mongo_url , {useNewUrlParser : true , useUnifiedTopology : true})
+
+const connection = mongoose.connection
+
+connection.on('error', err => console.log(err))
+
+connection.on('connected' , () => console.log('Mongo DB Connection Successfull'))
